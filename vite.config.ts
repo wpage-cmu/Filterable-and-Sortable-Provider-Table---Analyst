9462eaa30c741e04cf471f53d646a7b70e0f9539
wpage <wpage@andrew.cmu.edu>
2025-07-11 15:14:47 -0400

    package changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress certain warnings
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
